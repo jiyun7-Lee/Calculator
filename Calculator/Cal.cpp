b644028cfe8b616546c98eb05ed679f9ff84cd0e
jiyun7-Lee <leejiun7@naver.com>
2026-05-27 15:49:23 +0900

    cal 기능 구현

--- Calculator/Cal.cpp
+++ Calculator/Cal.cpp
@@ -3,13 +3,21 @@
 using namespace std;
 class Cal {
 public:
-	int result = 0;
+	int getSum(int a, int b)
+	{
+		return a + b;
+	}
+
+	int getMul(int a, int b) { return a * b; }
+	int getSub(int a, int b) { return a - b; }
+	int getDiv(int a, int b) { return a / b; }
 private:
+	int result = 0;
 };
 
 
 int main()
 {
 	Cal* cal = new Cal();
-	cout << cal->result;
+	cout << cal->getSum(1, 2);
 }
