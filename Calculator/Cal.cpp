2f622cc2fa9b3b68b84776e01d17e7919a3d1cbe
jiyun7-Lee <leejiun7@naver.com>
2026-05-27 15:45:10 +0900

    conflict

--- Calculator/Cal.cpp
+++ Calculator/Cal.cpp
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
+
 #include <iostream>
 using namespace std;
 class Cal {
 public:
 	int result = 0;
 private:
-
-=======
-class Cal {
->>>>>>> 54cd70a8fe26a4ccbde1759b06481a8ebab1a379
-
 };
+
 
 int main()
 {
-<<<<<<< HEAD
 	Cal* cal = new Cal();
 	cout << cal->result;
-=======
-
->>>>>>> 54cd70a8fe26a4ccbde1759b06481a8ebab1a379
 }
