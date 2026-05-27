c619b095e976c4530ae6268b6b076f2bad9755bd
jiyun7-Lee <leejiun7@naver.com>
2026-05-27 15:41:50 +0900

    print result

--- Calculator/Cal.cpp
+++ Calculator/Cal.cpp
@@ -1,11 +1,15 @@
+#include <iostream>
+using namespace std;
 class Cal {
-
+public:
+	int result = 0;
 private:
-	int result;
+
 
 };
 
 int main()
 {
-
+	Cal* cal = new Cal();
+	cout << cal->result;
 }
